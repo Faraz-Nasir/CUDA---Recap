84262828b4f1804e552cee539173a297afb39c99
Faraz Nasir <faraznasir20@gmail.com>
2026-01-15 12:18:28 -0500

    Macros

---
+++ 04 Macros/1.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+
+#define PI 3.14159
+#define AREA(r) (PI*r*r)
+
+#ifndef radius
+    #define radius 7
+#endif
+
+#if radius > 10
+    #define radius 10
+#elif radius < 5
+    #define radius 5
+#else
+    #define radius 7
+#endif
+
+int main(){
+    printf("Area of circle with radius %d:%f\n",radius,AREA(radius));
+}
